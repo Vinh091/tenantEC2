e0dd4fe0e773a5fcaa19995442caa754b0aa3fb5
Vinh091 <quangvinh10a22@gmail.com>
2024-12-22 19:11:13 +0700

    Update Basic RBAC

--- a_tenant_manager/models.py
+++ a_tenant_manager/models.py
@@ -12,10 +12,16 @@ class Tenant(TenantMixin):
 class Domain(DomainMixin):
     pass
 class TenantMember(models.Model):
+    ROLE_CHOICES = [
+        ('manager', 'Manager'),
+        ('designer', 'Designer'),
+        ('programmer', 'Programmer'),
+    ]
     tenant = models.ForeignKey(Tenant, on_delete=models.CASCADE, related_name="members")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_admin = models.BooleanField(default= False )
-    
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES, blank=True, default='') 
+
     class Meta: 
         unique_together = ('user', 'tenant')
     def __str__(self):
